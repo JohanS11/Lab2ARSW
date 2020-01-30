81b7b7c8115968c5316e8307cd652f1132e89f05
JohanS11 <johan.arias@mail.escuelaing.edu.co>
2020-01-30 09:12:12 -0500

    Punto 1 y 2

---
+++ src/main/java/edu/eci/arst/concprg/prodcons/Consumer.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arst.concprg.prodcons;
+
+import java.util.Queue;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class Consumer extends Thread{
+    
+    private Queue<Integer> queue;
+    
+    
+    public Consumer(Queue<Integer> queue){
+        this.queue=queue;        
+    }
+    
+    @Override
+    public void run() {
+        while (true) {
+
+            if (queue.size() > 0) {
+                synchronized (queue){
+                    try {
+                        queue.wait();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                int elem = queue.poll();
+                System.out.println("Consumer consumes "+elem);                                
+            }
+            
+        }
+    }
+}
